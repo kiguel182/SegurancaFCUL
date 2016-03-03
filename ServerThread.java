cb4cd856a5f36abb24f37a5ab0a6dd66459a758a
Miguel Condez <migalhasvcdez@gmail.com>
2016-03-03 16:21:54 +0000

    receive added

--- ServerThread.java
+++ ServerThread.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -47,4 +48,27 @@ import java.net.Socket;
 			}
 	}
 		
+		private void receive(ObjectInputStream inStream) throws IOException, ClassNotFoundException{
+			//vai ter de receber nome primeiro antes de criar o ficheiro
+			File result = new File("result.txt");
+			int fileArraySize = inStream.readInt();
+			byte[] fullByteFile = new byte[fileArraySize];
+			int ciclos = fileArraySize/1024;
+			int currentByte = 0;
+			
+			for(int i = 0; i <= ciclos; i++){
+				byte[] receive = new byte[1024];
+				receive = (byte[]) inStream.readObject();
+				for(int a = 0; a < 1024; a++){
+					if(currentByte < fileArraySize){
+						fullByteFile[currentByte] = receive[a];
+						currentByte++;
+					}
+				}
+			}
+			
+			FileOutputStream stream = new FileOutputStream(result);
+			stream.write(fullByteFile);
+			stream.close();	
+		}
 }
