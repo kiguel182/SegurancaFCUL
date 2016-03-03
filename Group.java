568c202a240d71caca35cc501d82f17593b500f6
FilipeP13 <filipe.pereira.1995@gmail.com>
2016-03-03 15:07:59 +0000

    Added files via upload
    mais group

---
+++ Group.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class Group {
+	
+	private File group;
+	private String user;
+
+	public Group(String user, File group) {
+		this.user = user;
+		this.group = group;
+	}
+
+}
--- Mensagem.java
+++ Mensagem.java
@@ -1,4 +1,28 @@
+import java.io.File;
 
 public class Mensagem {
+	
+	private String contact;
+	private String message;
+
+	public Mensagem(String contact, String message) {
+		this.contact = contact;
+		this.message = message;
+	}
+	
+	public boolean sendMessage(String contact, String message) {
+		
+		boolean send = false;
+		
+		if(contact != null && message != null) {
+			
+			send = true;
+		}
+		
+		return send;
+		
+	}
+	
+	
 
 }
