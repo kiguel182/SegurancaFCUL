4dbf00ca4511864364b12d1d6c3407b387f3dd43
Ricardo <root@kali.lan>
2016-03-08 15:56:15 +0000

    Login actualizado
    existingUser;
    existsUser;
    wrongPassword;

--- Login.java
+++ Login.java
@@ -5,15 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Login {
 
 	private String user;
 	private String pass;
+	private ArrayList atividades;
 
 	public Login(String user, String pass) {
 		this.user = user;
 		this.pass = pass;
+		this.atividades = new ArrayList();
 	}
 
 	void autenthicator(String user,String passwd) throws IOException{
@@ -40,7 +43,15 @@ public class Login {
 			}
 		}
 	}
-
+	/**
+	 * Verifica que um user com uma password 
+	 * no ficheiro
+	 * @param user utilizador a verificar
+	 * @param pwd password a verificar 
+	 * @param file ficheiro da base de dados
+	 * @return devolve falso se o utilizador com a passsword existir
+	 * @throws IOException
+	 */
 	private boolean existingUser(String user, String pwd,File file) throws IOException {
 
 		
@@ -69,6 +80,46 @@ public class Login {
 		}
 		return existUser;
 
-	}	
+	}
+	/**
+	 * Verificar que um utilizador existe 
+	 * @param user utilizador a verificar
+	 * @param file ficheiro com a lista de passwords
+	 * @return devolve verdadeiro se o utilizador existir
+	 * @throws IOException
+	 */
+	public boolean userExists(String user,File file) throws IOException{
+		boolean userExists = false;
+		String str = null;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while((str = br.readLine()) != null){
+				String[] userF = str.split(":");
+				if(userF[0].equals(user)){
+					userExists = true;
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return userExists;
+		
+	}
+	/**
+	 * Metodo que verifica 
+	 * se a password do utilizador 
+	 * esta correcta
+	 * @param user utilizador para o qual se quer verificar a password
+	 * @param pwd password que se quer verificar a password
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean wrongPassword(String user,String pwd,File file) throws IOException{
+		return userExists(user,file) && !existingUser(user,pwd,file);
+	}
 
 }
